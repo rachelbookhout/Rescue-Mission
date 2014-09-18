df4b8ba1e47f53f40b25b0ff1288766d87666d86
rachelbookhout <rbookhou@gmail.com>
2014-09-18 07:53:11 -0400

    Working answer error messages

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,8 +3,12 @@ class AnswersController < ApplicationController
     @question = Question.find(params[:question_id])
     @answer = Answer.new(answer_params)
     @answer.question_id = @question.id
-    @answer.save
+    @answers = Answer.all.order(created_at: :desc).where(question_id:"#{@question.id}")
+    if @answer.save == false
+    render 'questions/show'
+    else
     redirect_to @question
+    end
   end
 
  private
